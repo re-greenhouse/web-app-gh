303b19d6219138d0690b4d96422098e2bf8feded
Andrés Leiva <aleiva1700@gmail.com>
2024-04-23 16:51:00 -0500

    feat(navigation): add auth paths in router

--- src/router/components/AppRouter.tsx
+++ src/router/components/AppRouter.tsx
@@ -1,11 +1,15 @@
 import {ReactElement} from "react";
 import {Route, Routes} from "react-router-dom";
 import {HomePage} from "@/public/pages/HomePage.tsx";
+import {LoginPage} from "@/auth/pages/LoginPage.tsx";
+import {RegisterPage} from "@/auth/pages/RegisterPage.tsx";
 
 export const AppRouter = (): ReactElement => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
 };
