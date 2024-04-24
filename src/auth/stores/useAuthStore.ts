c61c9ec764c2734c6612a0e6f798b0cafd46cc7d
Andrés Leiva <aleiva1700@gmail.com>
2024-04-24 13:59:40 -0500

    feat(auth-domain): add useAuthStore

---
+++ src/auth/stores/useAuthStore.ts
@@ -0,0 +1,19 @@
+import {create} from "zustand";
+import {persist} from "zustand/middleware";
+import {Profile} from "@/auth/models/Profile.ts";
+
+type AuthStore = {
+  token?: string,
+  profile?: Profile,
+  updateToken: (token: string) => void,
+  updateProfile: (profile: Profile) => void,
+}
+
+export const useAuthStore = create<AuthStore>()(persist(((set) => ({
+  token: undefined,
+  profile: undefined,
+  updateToken: (token: string) => set(() => ({ token: token })),
+  updateProfile: (profile: Profile) => set(() => ({ profile: profile })),
+})), {
+  name: "user"
+}));
