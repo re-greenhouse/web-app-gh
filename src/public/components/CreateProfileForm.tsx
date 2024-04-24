f6a24f4345d247bf9d3f2253ff42439b1a047cb7
Andrés Leiva <aleiva1700@gmail.com>
2024-04-24 14:21:47 -0500

    feat(public): add create profile page and form

---
+++ src/public/components/CreateProfileForm.tsx
@@ -0,0 +1,9 @@
+import {ReactElement} from "react";
+
+export const CreateProfileForm = (): ReactElement => {
+  return (
+    <div className="py-8">
+      <p>Placeholder</p>
+    </div>
+  );
+};
---
+++ src/public/pages/CreateProfilePage.tsx
@@ -0,0 +1,14 @@
+import {ReactElement} from "react";
+import {CreateProfileForm} from "@/public/components/CreateProfileForm.tsx";
+
+export const CreateProfilePage = (): ReactElement => {
+  return (
+    <main className="flex flex-col justify-around items-center bg-light p-6 min-h-dvh">
+      <div className="w-full">
+        <h1 className="text-4xl font-bold text-primary">Cree su perfil:</h1>
+        <p className="font-medium text-gray-600">Antes de comenzar, cuéntanos un poco acerca de ti.</p>
+        <CreateProfileForm />
+      </div>
+    </main>
+  );
+};
