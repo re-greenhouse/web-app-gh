6c13cf5310b720512ab6930241ad30f629f98ea5
Andrés Leiva <aleiva1700@gmail.com>
2024-04-24 23:05:07 -0500

    feat(profile-service): add profile retrieving function

---
+++ src/profiles/services/profile.service.ts
@@ -0,0 +1,31 @@
+import {Profile} from "@/profiles/models/Profile.ts";
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_ENDPOINT
+});
+
+
+export const getMyProfile = async(token: string): Promise<{status: string; message: string; payload?: Profile}> => {
+  try {
+    const response = await instance.get("/profiles/users/me",
+      { headers: {Authorization: `Bearer ${token}`} }
+    );
+    return {
+      status: "success",
+      message: "Perfil recuperado con éxito.",
+      payload: {
+        id: response.data["id"],
+        userId: response.data["userId"],
+        firstName: response.data["firstName"],
+        lastName: response.data["lastName"],
+        iconUrl: response.data["iconUrl"],
+      }
+    };
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error) && error.response?.status == 404) {
+      return { status: "redirect", message: "Redirigiendo a la creación de usuario." };
+    }
+    return { status: "error", message: "Hubo un problema con el servidor. Intente de nuevo en unos minutos." };
+  }
+}
