d2a096675eda75e68f67bd61bd61c5dda749fd71
Andrés Leiva <aleiva1700@gmail.com>
2024-04-24 23:20:55 -0500

    feat(router): add router logic for handling token and profile

--- src/router/components/AppRouter.tsx
+++ src/router/components/AppRouter.tsx
@@ -3,13 +3,27 @@ import {Route, Routes} from "react-router-dom";
 import {HomePage} from "@/public/pages/HomePage.tsx";
 import {LoginPage} from "@/auth/pages/LoginPage.tsx";
 import {RegisterPage} from "@/auth/pages/RegisterPage.tsx";
+import {useAuthStore} from "@/auth/stores/useAuthStore.ts";
+import {PrivateRoute} from "@/router/components/PrivateRoute.tsx";
+import {CreateProfilePage} from "@/profiles/pages/CreateProfilePage.tsx";
 
 export const AppRouter = (): ReactElement => {
+  const {token, profile} = useAuthStore((state) => ({token: state.token, profile: state.profile}));
+
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
+      <Route element={<PrivateRoute canActivate={token !== undefined} defaultDestination="/login" />}>
+        <Route element={<PrivateRoute canActivate={profile !== undefined} defaultDestination="/crear-perfil" />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
+        <Route element={<PrivateRoute canActivate={profile === undefined} defaultDestination="/" />}>
+          <Route path="/crear-perfil" element={<CreateProfilePage />} />
+        </Route>
+      </Route>
+      <Route element={<PrivateRoute canActivate={token === undefined} defaultDestination="/" />}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
     </Routes>
   );
 };
