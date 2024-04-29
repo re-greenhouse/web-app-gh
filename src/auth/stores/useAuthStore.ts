f9777b75806a6e9a276ad4ecc15332c560c6b2ae
Andrés Leiva <aleiva1700@gmail.com>
2024-04-29 10:34:41 -0500

    refactor(auth-store): update auth store with better and simple methods

--- src/auth/stores/useAuthStore.ts
+++ src/auth/stores/useAuthStore.ts
@@ -1,19 +1,21 @@
 import {create} from "zustand";
 import {persist} from "zustand/middleware";
-import {Profile} from "@/profiles/models/Profile.ts";
+import {Profile} from "@/auth/models/Profile.ts";
 
 type AuthStore = {
   token?: string,
   profile?: Profile,
-  updateToken: (token: string) => void,
-  updateProfile: (profile: Profile) => void,
+  login: (token: string, profile: Profile) => void;
+  logout: () => void;
+  isLoggedIn: () => boolean;
 }
 
-export const useAuthStore = create<AuthStore>()(persist(((set) => ({
+export const useAuthStore = create<AuthStore>()(persist(((set, get) => ({
   token: undefined,
   profile: undefined,
-  updateToken: (token: string) => set(() => ({ token: token })),
-  updateProfile: (profile: Profile) => set(() => ({ profile: profile })),
+  login: (token: string, profile: Profile) => set(() => ({ token: token, profile: profile })),
+  logout: () => set(() => ({ token: undefined, profile: undefined })),
+  isLoggedIn: () => get().token !== undefined,
 })), {
   name: "user"
 }));
--- src/router/components/AppRouter.tsx
+++ src/router/components/AppRouter.tsx
@@ -5,22 +5,16 @@ import {LoginPage} from "@/auth/pages/LoginPage.tsx";
 import {RegisterPage} from "@/auth/pages/RegisterPage.tsx";
 import {useAuthStore} from "@/auth/stores/useAuthStore.ts";
 import {PrivateRoute} from "@/router/components/PrivateRoute.tsx";
-import {CreateProfilePage} from "@/profiles/pages/CreateProfilePage.tsx";
 
 export const AppRouter = (): ReactElement => {
-  const {token, profile} = useAuthStore((state) => ({token: state.token, profile: state.profile}));
+  const isLogged = useAuthStore((state) => state.isLoggedIn());
 
   return (
     <Routes>
-      <Route element={<PrivateRoute canActivate={token !== undefined} defaultDestination="/login" />}>
-        <Route element={<PrivateRoute canActivate={profile !== undefined} defaultDestination="/crear-perfil" />}>
-          <Route path="/" element={<HomePage />} />
-        </Route>
-        <Route element={<PrivateRoute canActivate={profile === undefined} defaultDestination="/" />}>
-          <Route path="/crear-perfil" element={<CreateProfilePage />} />
-        </Route>
+      <Route element={<PrivateRoute canActivate={isLogged} defaultDestination="/login" />}>
+        <Route path="/" element={<HomePage />} />
       </Route>
-      <Route element={<PrivateRoute canActivate={token === undefined} defaultDestination="/" />}>
+      <Route element={<PrivateRoute canActivate={!isLogged} defaultDestination="/" />}>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Route>
