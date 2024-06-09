dc90038c65a0c387f996b8c8a55e4e8b8e5339d3
Andrés Leiva <aleiva1700@gmail.com>
2024-06-09 12:46:14 -0500

    refactor(auth-store): remove username and password from store

--- src/auth/stores/useAuthStore.ts
+++ src/auth/stores/useAuthStore.ts
@@ -5,9 +5,7 @@ import {Profile} from "@/auth/models/Profile.ts";
 type AuthStore = {
   token?: string,
   profile?: Profile,
-  username?: string,
-  password?: string,
-  login: (token: string, profile: Profile, username: string, password: string) => void;
+  login: (token: string, profile: Profile) => void;
   logout: () => void;
   isLoggedIn: () => boolean;
 }
@@ -15,10 +13,8 @@ type AuthStore = {
 export const useAuthStore = create<AuthStore>()(persist(((set, get) => ({
   token: undefined,
   profile: undefined,
-  username: undefined,
-  password: undefined,
-  login: (token: string, profile: Profile, username: string, password: string) => set(() => ({ token, profile, username, password })),
-  logout: () => set(() => ({ token: undefined, profile: undefined, username: undefined, password: undefined })),
+  login: (token: string, profile: Profile) => set(() => ({ token, profile })),
+  logout: () => set(() => ({ token: undefined, profile: undefined })),
   isLoggedIn: () => get().token !== undefined,
 })), {
   name: "user"
