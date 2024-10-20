37f9d0c946446d79dd48c427efa3048a808315c9
asotito231 <u202116113@upc.edu.pe>
2024-10-20 15:33:25 -0500

    feat: added responsive view to crops in progress

--- src/shared/components/Banner.tsx
+++ src/shared/components/Banner.tsx
@@ -8,15 +8,15 @@ export const BannerComponent= (): ReactElement => {
 
     return (
         <div className="relative">
-            <img src="public/bannerImage.svg" alt="bannerImage" className="absolute w-full"/>
-            <div className="relative z-10 flex items-end px-28 pt-32 pb-16 gap-5">
+            <img src="public/bannerImage.svg" alt="bannerImage" className="absolute w-full hidden md:block"/>
+            <div className="relative z-10 flex md:flex-row flex-col md:items-end items-center text-center md:px-28 px-12 md:pt-32 pb-10 md:gap-5 gap-2">
                 <img
                     src={company?.logoUrl}
                     alt={`${company?.name} logo`}
-                    className="size-24 md:size-48 object-cover object-center bg-no-repeat bg-transparent rounded-2xl border-2 border-secondary"
+                    className="size-32 md:size-48 object-cover object-center bg-no-repeat bg-transparent rounded-2xl border-2 border-secondary"
                 />
                 <div className="items-end">
-                    <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold mb-2 text-third">
+                    <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold mb-2 text-third whitespace-nowrap">
                         {company?.name}
                     </h2>
                     <h2 className="text-base sm:text-xs md:text-sm lg:text-lg text-secondary">
