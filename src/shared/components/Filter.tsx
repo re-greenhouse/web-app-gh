71c60bd1fba5758c116d86f88dda8934a023d1c3
asotito231 <u202116113@upc.edu.pe>
2024-11-12 18:57:29 -0500

    feat: changed filter component to recieve react component

--- src/shared/components/Filter.tsx
+++ src/shared/components/Filter.tsx
@@ -3,32 +3,23 @@ import { ReactElement } from "react";
 type FilterProps = {
   label: string;
   onClick: () => void;
-  showArrow: boolean;
-  leadingIcon?: string;
+  leadingIcon?: ReactElement;
   color?: string;
 };
 
 export const Filter = ({
   label,
   onClick,
-  showArrow,
   leadingIcon,
-  color,
+  color
 }: FilterProps): ReactElement => {
   return (
     <div
       className="inline-flex items-center gap-3 cursor-pointer"
       onClick={onClick}
     >
-      {leadingIcon && <img src={leadingIcon} alt="icon" className="w-6 h-6" />}
       <p style={{ color: color || "#898989" }}>{label}</p>
-      <img
-        src="/icons/downArrow.svg"
-        alt="arrow"
-        className={`w-4 h-4 transition-transform duration-300 ${
-          showArrow ? "rotate-180" : ""
-        }`}
-      />
+      {leadingIcon}
     </div>
   );
 };
