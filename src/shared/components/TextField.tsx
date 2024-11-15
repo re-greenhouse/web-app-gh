eed21e8be06254ab8006a60e6ee93524860588b3
trabajosUPC-AlanGalavis <u202110223@upc.edu.pe>
2024-11-14 19:55:35 -0500

    fix: Fix issue with trailing icon on TextField

--- src/shared/components/TextField.tsx
+++ src/shared/components/TextField.tsx
@@ -44,7 +44,7 @@ export const TextField = ({
           name={id}
           className={`
             bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ${
-              TrailingIcon !== undefined && "ps-10"
+              trailingIcon !== undefined && "ps-10"
             } p-2.5
           `}
           placeholder={placeholder ?? ""}
